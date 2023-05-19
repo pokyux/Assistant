2987041ead92f503108b402f51761db7f03cffdf
Pokyux <shiftregister233@outlook.com>
2023-05-19 15:47:53 +0800

    add: is normal user

--- processors/auth.go
+++ processors/auth.go
@@ -7,6 +7,16 @@ import (
 	"github.com/pokyux/Assistant/conf"
 )
 
+func IsNormalUser(_id int64) bool {
+	for _, id := range conf.NormalUsers {
+		if id == _id {
+			return true
+		}
+	}
+
+	return false
+}
+
 func WhoAmI(rcvd *tgbotapi.Message, rply *tgbotapi.MessageConfig) {
 	rply.Text = fmt.Sprintf("Your ID is: %d, and you ", rcvd.From.ID)
 	if rcvd.From.ID == conf.SuperAdmin {
