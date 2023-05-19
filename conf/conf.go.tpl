f719bd77efd0d8814e6eeb1e9afad0cc74271ba5
Pokyux <shiftregister233@outlook.com>
2023-05-19 14:46:49 +0800

    init conf

---
+++ conf/conf.go.tpl
@@ -0,0 +1,13 @@
+package conf
+
+const SuperAdmin = 5637757591
+
+var NormalUsers = []int64{5637757591}
+
+// OSS Config
+const (
+	OSSEndpoint        = ""
+	OSSAccessKeyId     = ""
+	OSSAccessKeySecret = ""
+	OSSBucketName      = ""
+)
--- processors/auth.go
+++ processors/auth.go
@@ -4,13 +4,12 @@ import (
 	"fmt"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/pokyux/Assistant/conf"
 )
-
-const SuperAdmin = 5637757591
 
 func WhoAmI(rcvd *tgbotapi.Message, rply *tgbotapi.MessageConfig) {
 	rply.Text = fmt.Sprintf("Your ID is: %d, and you ", rcvd.From.ID)
-	if rcvd.From.ID == SuperAdmin {
+	if rcvd.From.ID == conf.SuperAdmin {
 		rply.Text += "are my admin."
 	} else {
 		rply.Text += "are not my admin."
